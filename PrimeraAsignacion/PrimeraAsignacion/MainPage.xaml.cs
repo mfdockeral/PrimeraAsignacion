53fc8db0b57252a72161de23cce904808d4f0afb
Mario Rafael Felipe Rondón <mr.felipe1815@hotmail.com>
2020-08-20 20:03:54 -0400

    Agregar archivos de proyecto.

---
+++ PrimeraAsignacion/PrimeraAsignacion/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PrimeraAsignacion
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		//Evento del botón mostrar.
+		private void btnMostrar_Clicked(object sender, EventArgs e)
+		{
+			var texto = TxtNom.Text;
+			this.DisplayAlert("Bienvenido a nuestra primera asignación", texto, "Ok");
+			TxtNom.Text = "";
+		}
+	}
+}
